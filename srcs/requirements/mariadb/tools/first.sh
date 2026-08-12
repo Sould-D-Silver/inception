f708261bb8401fa1f3009b63ffcc513ebdf7ab49
sjoao <sjoao@archlinux>
2026-08-12 16:21:54 +0000

    test: tentando fazer so criar a bd se ja nao existir

--- srcs/requirements/mariadb/tools/first.sh
+++ srcs/requirements/mariadb/tools/first.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+
+if [ ! -d "/var/lib/mysql" ]; then
+    mysql_install_db --user=mysql && echo "criou   o banco de dados";
+    mysqld_safe --skip-networking & echo "iniciou o servidor";
+    mysql  < /bin/db_init.sql && echo "inseriu os dados iniciais";
+    mysqladmin shutdown && echo "desligou o servidor";
+
+fi
 
 
-mysql_install_db --user=mysql && echo "criou   o banco de dados";
-mysqld_safe --skip-networking & echo "iniciou o servidor";
-
-mysql  < /bin/db_init.sql && echo "inseriu os dados iniciais";
-
-mysqladmin shutdown && echo "desligou o servidor";
 echo "iniciando o servidor novamente";
 exec mysqld ;
