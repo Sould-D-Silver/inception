9f01ddcb16837364e9673da2cd34e06e7cda2e58
sjoao <sjoao@archlinux>
2026-08-14 14:47:23 +0000

    cleanup: coloquei as infos na env

--- srcs/requirements/wordpress/tools/inicial.sh
+++ srcs/requirements/wordpress/tools/inicial.sh
@@ -11,43 +11,47 @@ wait_for_db() {
 }
 
 
-
 if [ ! -f "/var/www/html/wp-config.php" ]; then
 
-   # wait_for_db
+    if [ -f "/var/www/html/wp-config-sample.php" ]; then
+        cp  wp-config-sample.php wp-config-sample.php
+    fi
+
     echo "baixando e configurando o wordpress"
     if ! wp core is-installed  ; then
         wp core download --allow-root
     fi
-    wp config create \
-    --dbname="$MYSQL_DATABASE" \
-    --dbuser="$MYSQL_USER" \
-    --dbpass="$MYSQL_PASSWORD" \
-    --dbhost="mariadb" \
-    --allow-root
+
+    wait_for_db
+
+     wp config create \
+        --dbname="$MYSQL_DATABASE" \
+        --dbuser="$MYSQL_USER" \
+        --dbpass="$MYSQL_PASSWORD" \
+        --dbhost="$DB_HOST" \
+        --allow-root
+
+    echo "dps do create"
 
     if [ ! -f "/var/www/html/wp-config.php" ]; then
         echo "Erro: wp-config.php não foi criado corretamente."
-        #exit 1
+        exit 1
     fi
 
     wp core install \
-    --title="Inception" \
-    --url="https://sjoao.42.fr" \
-    --admin_user="$WP_ADMIN_USER" \
-    --admin_password="$WP_ADMIN_PASSWORD" \
-    --admin_email="wp-admin@sjoao.42.fr" \
-    --allow-root
+        --title="$WP_TITLE" \
+        --url="$WP_URL" \
+        --admin_user="$WP_ADMIN_USER" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --allow-root
 
+    echo "dps do core install"
 
     wp user create "$WP_USER" "$WP_USER_EMAIL" \
-    --role=author \
-    --user_pass="$WP_USER_PASSWD" \
-    --allow-root
-
-
-
-    
+        --role=author \
+        --user_pass="$WP_USER_PASSWD" \
+        --allow-root
 
 else
     echo "wordpress já está baixado e cconfigurado" 
